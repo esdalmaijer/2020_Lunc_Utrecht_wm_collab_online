d2023e629067a16ba5bf3d594610a7b3c96a23b8
AliekeStruik <aliekestruik@gmail.com>
2020-11-19 13:49:25 +0100

    Coins+scene03

---
+++ memory_collaboration_game/Assets/Scripts/Coin.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+   
+    // variables 
+    private float speed = 30.0f;
+    public Vector2 target = new Vector2(7f, -5f);
+    // bool Scene03 = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void changetarget(bool Scene03)
+    {
+        if(Scene03 == true)
+        {
+           
+            target.x = target.x -1;
+            
+            target.y = -4f;
+        }
+        if(Scene03 == false)
+        {
+            target.x = 9f;
+            target.y = -5f;
+        }
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        Vector2 endPosition = new Vector2(position.x,-2);
+
+        if(position != endPosition) {
+            //position.y = position.y - 0.05f;
+            //transform.position = position;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+        }
+    }
+
+   
+
+   
+}
---
+++ memory_collaboration_game/Assets/Scripts/ScriptScene03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptScene03 : MonoBehaviour
+{
+    [SerializeField] Treasure_Box Treasure_BoxPrefab;
+    [SerializeField] Coin feedbackPrefab;
+
+    private static int totalcoins = MemTestManager.nCoins;
+    //private static int totalcoins = 2;
+    private List<Coin> coinList = new List<Coin>();
+
+    
+
+    Vector3 coinposition = new Vector3(-6, 3, 0);
+
+    void Start()
+    {
+        Vector3 boxposition = new Vector3(-8, 0, 0);
+        Instantiate(Treasure_BoxPrefab, boxposition, transform.rotation);
+
+        for (int i = 0; i < totalcoins; i++)
+        {
+            coinposition.x = coinposition.x + i;
+            //Coin.target = Coin.target.y +1f;
+            feedbackPrefab.changetarget(true);
+            coinList.Add(Instantiate(feedbackPrefab, coinposition, transform.rotation));
+        }
+    }
+    void Update()
+    {
+        
+    } 
+}
+
